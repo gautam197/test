dcb2c80529f9d242ab683bf39ee1ab96f9162dde
Gautam Phuyal <gautamphuyal017@sxc.edu.np>
2020-04-23 12:33:46 +0545

    port changed to heroku server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
-
+var middleware = require('./middleware.js');
+var port = process.env.PORT || 3000;
 // }
 // app.use(middleware.requireAuthentication);
 app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send("<h1> hello youtube </h1>");
 });
 app.use(express.static(__dirname + '/public'));
-app.listen(3000);	
+app.listen(port, function(){
+	console.log('Express server started in port ' + port + '!!!');
+});	
